cfde9976bb425da24bd7355d9c6357263629779f
clairesquires99 <clairesquires99@outlook.com>
2021-10-16 14:42:23 +0100

    dunno

--- view/views.py
+++ view/views.py
@@ -1,4 +1,6 @@
 from django.views.generic.base import TemplateView
+from .models import Organisation
 
 class ViewPageView(TemplateView):
+    model = Organisation
     template_name = "view/view.html"
