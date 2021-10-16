ea9c58c54e29c356118f3453c2f1625e0c235e2d
Sandra Tu <sandratu2000@gmail.com>
2021-10-16 13:19:43 +0100

    fixed admin

--- register/admin.py
+++ register/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import Organisation
 
 @admin.register(Organisation)
-class OrganisationAdmin(Organisation):
-    list_display = ['name', 'country', 'city']
+class OrganisationAdmin(admin.ModelAdmin):
+    list_display = ['name', 'country', 'city', 'type']
