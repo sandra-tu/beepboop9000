7d82631962dfc7b5cfcb8c0368a88755ff0df94e
Sandra Tu <sandratu2000@gmail.com>
2021-10-16 22:29:59 +0100

    updated report form

--- view/views.py
+++ view/views.py
@@ -3,6 +3,7 @@
 from django.shortcuts import render
 from django.http import Http404
 from register.models import Organisation
+from django.views.generic import ListView
 #
 # class ViewPageView(TemplateView):
 #     model = Organisation
@@ -13,3 +14,18 @@ def view_view(request):
     return render(request, 'view/view.html', {
         'orgs': orgs,
     })
+
+class SearchView(ListView):
+    model = Organisation
+    template_name = 'view.html'
+    # context_object_name = 'all_search_results'
+
+    def get_queryset(self):
+       result = super(SearchView, self).get_queryset()
+       query = self.request.GET.get('search')
+       if query:
+          postresult = Organisation.objects.filter(name__contains=query)
+          result = postresult
+       else:
+           result = None
+       return result
