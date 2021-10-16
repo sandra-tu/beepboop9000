97b77ffa877081fe2e3752f26dd54c07ef7507db
Sandra Tu <sandratu2000@gmail.com>
2021-10-16 12:50:21 +0100

    added organisation admin

--- register/admin.py
+++ register/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Organisation
 
 # Register your models here.
+class OrganisationAdmin(Organisation):
+    list_display('name', 'country', 'city')
