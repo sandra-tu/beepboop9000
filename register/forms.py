3bae9e0bb79c5e7042898d6138c1fdc46b12ca24
clairesquires99 <clairesquires99@outlook.com>
2021-10-16 22:00:18 +0100

    fixing merge

--- register/forms.py
+++ register/forms.py
@@ -1,31 +1,15 @@
 from django import forms
 from .models import Organisation
 
-<<<<<<< HEAD
-COUNTRY_CHOICES = [
-        ('gb','Great Britain'),
-        ('ie', 'Ireland'),
-    ]
-TYPE_CHOICES = [
-    ('pubbar', 'Pub/Bar'),
-    ('club', 'Nightclub'),
-    ('soc', 'University Society'),
-    ('school', 'School'),
-    ('office', 'Office'),
-    ('store', 'Store'),
-    ('other', 'Other'),
-]
-CITY_CHOICES = [('edi', 'Edinburgh')]
-
-class RegisterForm(forms.Form):
-=======
 class RegisterForm(forms.ModelForm):
->>>>>>> ad4f752 (starting report form)
     # class Meta:
     #     model = Organisation
     #     feilds = ('name', 'country', 'city', 'type', 'email')
-    name = forms.CharField(max_length=255, required=False)
-    country = forms.CharField(label='Country', widget=forms.Select(choices=COUNTRY_CHOICES))
-    city = forms.CharField(label='Country', widget=forms.Select(choices=CITY_CHOICES))
-    type = forms.CharField(label='Country', widget=forms.Select(choices=TYPE_CHOICES))
-    email = forms.CharField(label='Email', max_length=255, required=True)
+    # name = forms.CharField(max_length=255, required=False)
+    # country = forms.CharField(label='Country', widget=forms.Select(choices=COUNTRY_CHOICES))
+    # city = forms.CharField(label='Country', widget=forms.Select(choices=CITY_CHOICES))
+    # type = forms.CharField(label='Country', widget=forms.Select(choices=TYPE_CHOICES))
+    # email = forms.CharField(label='Email', max_length=255, required=True)
+    class Meta:
+        model = Organisation
+        fields = ('name', 'country', 'city', 'type', 'email')
--- report/forms.py
+++ report/forms.py
@@ -4,4 +4,4 @@ from .models import Report
 class ReportForm(forms.ModelForm):
     class Meta:
         model = Report
-        fields = ('date', 'time', 'location', 'type', 'email')
+        fields = ('date', 'time')
