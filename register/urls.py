558569797b84af5a1357330cd0ee90b94917a5cf
Sandra Tu <sandratu2000@gmail.com>
2021-10-16 17:49:19 +0100

    registration form semi-working

--- register/urls.py
+++ register/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 # from .views import RegisterPageView
-from .views import register_view
+from .views import register_view, submit_view
 
 urlpatterns = [
-  path('register', register_view, name='register'),
+  path('register/', register_view, name='register'),
+  path('register_submit/', submit_view, name='submit')
 ]
--- register/views.py
+++ register/views.py
@@ -5,28 +5,43 @@ from .forms import RegisterForm
 from .models import Organisation
 
 def register_view(request):
-    if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            form_name = form.cleaned_data['name']
-            form_country = form.cleaned_data['email']
-            form_city = form.cleaned_data['author']
-            form_type = form.cleaned_data['avatar']
-            form_email = form.cleaned_data['title']
-
-            organisation_name = Organisation.objects.create(name=form_name)
-            organisation_country = Organisation.objects.create(name=form_country)
-            organisation_city = Organisation.objects.create(name=form_city)
-            organisation_type = Organisation.objects.create(name=form_type)
-            organisation_email = Organisation.objects.create(name=form_email)
-
-            # try:
-            #     send_mail('Story Submission', message, email,
-            #               ['info.controlfreaks@gmail.com'])
-            # except BadHeaderError:
-            #     return redirect('submit/failure')
-            # return redirect('submit/success')
-
     form = RegisterForm()
     return render(request, 'register/register.html', {'form': form})
 
+def submit_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        # if form.is_valid():
+        print('woooooo!')
+        # form_name = form.cleaned_data['name']
+        # form_country = form.cleaned_data['email']
+        # form_city = form.cleaned_data['author']
+        # form_type = form.cleaned_data['avatar']
+        # form_email = form.cleaned_data['title']
+        form_name = request.POST.get('name')
+        form_country = request.POST.get('country')
+        form_city = request.POST.get('city')
+        form_type = request.POST.get('type')
+        form_email = request.POST.get('email')
+
+        # organisation_name = Organisation.objects.create(name=form_name)
+        # organisation_country = Organisation.objects.create(name=form_country)
+        # organisation_city = Organisation.objects.create(name=form_city)
+        # organisation_type = Organisation.objects.create(name=form_type)
+        # organisation_email = Organisation.objects.create(name=form_email)
+        new_org = Organisation.objects.create(
+            name=form_name,
+            country=form_country,
+            city=form_city,
+            type = form_type,
+            email=form_email,
+        )
+
+        # try:
+        #     send_mail('Story Submission', message, email,
+        #               ['info.controlfreaks@gmail.com'])
+        # except BadHeaderError:
+        #     return redirect('submit/failure')
+        # return redirect('submit/success')
+
+    return render(request, 'register/submitted.html', {})
