ad4f75215eb8cde6667c8a475b8a3410a210887b
Sandra Tu <sandratu2000@gmail.com>
2021-10-16 20:39:39 +0100

    starting report form

--- report/models.py
+++ report/models.py
@@ -7,13 +7,15 @@ class Report(models.Model):
         ('customer', 'Customer'),
         ('employee', 'Employee'),
         ('client', 'Client'),
-        ('boss', 'Boss')
+        ('boss', 'Boss'),
+        ('other', 'Other'),
     ]
     date = models.DateField()
     time = models.TimeField(blank=True)
     accused_description = models.CharField(max_length=1000, blank=True)
-    location = models.CharField(blank=True, max_length=200)
+    # location = models.ForeignKey()
     repeat = models.BooleanField(default=False)
     name = models.CharField(blank=True, max_length=100)
+    event_description = models.CharField(blank=True, max_length=4000)
     accused_relationship = models.CharField(choices=RELATIONSHIP_CHOICES, blank=False, max_length=100)
     submitter_relationship = models.CharField(choices=RELATIONSHIP_CHOICES, blank=False, max_length=100)
--- report/views.py
+++ report/views.py
@@ -2,11 +2,13 @@
 from django.shortcuts import render
 from django.http import Http404
 from django.core.mail import send_mail
+from .forms import ReportForm
 
 # class ReportPageView(TemplateView):
 #     template_name = "report/report.html"
 
 def report_view(request):
+    form = ReportForm()
     send_mail(
         'uhhh',
         'HEY???',
@@ -14,4 +16,4 @@ def report_view(request):
         ['sandratu2000@gmail.com'],
         fail_silently=False,
     )
-    return render(request, 'report/report.html', {})
+    return render(request, 'report/report.html', {'form': form})
