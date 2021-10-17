552a5412a0040d349afaa8981cc65a0ea6880a00
Sandra Tu <sandratu2000@gmail.com>
2021-10-17 12:48:36 +0100

    added org foreign key

--- report/models.py
+++ report/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 from django.conf import settings
 
+from register.models import Organisation
+
 class Report(models.Model):
     RELATIONSHIP_CHOICES = [
         ('customer', 'Customer'),
@@ -13,7 +15,7 @@ class Report(models.Model):
     date = models.DateField()
     time = models.TimeField(blank=True)
     accused_description = models.CharField(max_length=1000, blank=True)
-    # location = models.ForeignKey()
+    location = models.ForeignKey(Organisation, on_delete=models.SET_NULL, null=True, blank=False)
     repeat = models.BooleanField(default=False)
     name = models.CharField(blank=True, max_length=100)
     event_description = models.CharField(blank=True, max_length=4000)
