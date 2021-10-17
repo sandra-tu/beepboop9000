c8bea8f81c831b549bb1447646e58bf1cd0597a7
clairesquires99 <clairesquires99@outlook.com>
2021-10-17 14:00:12 +0100

    placeholders and labels on forms added

--- report/forms.py
+++ report/forms.py
@@ -5,11 +5,16 @@ class ReportForm(forms.ModelForm):
     class Meta:
         model = Report
 
-        fields = ('date', 'time',  'accused_description', 'event_description', 'name', 'location')
+        fields = ('date', 'time',  'location', 'accused_description', 'event_description', 'name')
         widgets = {
             'date': forms.TextInput(attrs={'type': 'date'}),
             'time': forms.TextInput(attrs={'type': 'time'}),
+            'location': forms.TextInput(attrs={'placeholder': 'Select the organisation this happened in'}),
             'accused_description':forms.Textarea(attrs={'rows': 3, 'cols': 25, 'placeholder':'Describe the perpetrator in any way you feel comfortable'} ),
             'event_description':forms.Textarea(attrs={'rows': 5, 'cols': 25, 'placeholder':'Describe the event as fully as possible'}),
             'name': forms.TextInput(attrs={'placeholder': 'Optional: if you would like us to know who you are'}),
+        }
+        labels = {
+            'accused_description': 'Description of perpetrator',
+            'event_description': 'Description of event',
         }
