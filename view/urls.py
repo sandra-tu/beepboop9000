7c7151dbce232b0d5c959353097654fe90b91b55
clairesquires99 <clairesquires99@outlook.com>
2021-10-17 02:52:50 +0100

    search functionality working in view app, front-end changes

--- view/urls.py
+++ view/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import view_view
 
 urlpatterns = [
-  # path('view', ViewPageView.as_view(), name='view'),
+  # path('view', SearchView.as_view(), name='view'),
   path('view', view_view, name='view'),
 ]
--- view/views.py
+++ view/views.py
@@ -4,28 +4,45 @@ from django.shortcuts import render
 from django.http import Http404
 from register.models import Organisation
 from django.views.generic import ListView
+from django.db.models import Q
 #
 # class ViewPageView(TemplateView):
 #     model = Organisation
 #     template_name = "view/view.html"
 
 def view_view(request):
-    orgs = Organisation.objects.all()
-    return render(request, 'view/view.html', {
-        'orgs': orgs,
-    })
+    # orgs = Organisation.objects.all()
+    search_organisation = request.GET.get('search')
 
-class SearchView(ListView):
-    model = Organisation
-    template_name = 'view.html'
-    # context_object_name = 'all_search_results'
+    if search_organisation:
+        organisations = Organisation.objects.filter(Q(name__icontains=search_organisation))
+    else:
+        # If not searched, return default posts
+        organisations = Organisation.objects.all().order_by("name")
+    return render(request, 'view/view.html', {'orgs': organisations})
+    
 
-    def get_queryset(self):
-       result = super(SearchView, self).get_queryset()
-       query = self.request.GET.get('search')
-       if query:
-          postresult = Organisation.objects.filter(name__contains=query)
-          result = postresult
-       else:
-           result = None
-       return result
+# class SearchView(ListView):
+#     model = Organisation
+#     template_name = 'view.html'
+#     # context_object_name = 'all_search_results'
+
+#     # def get_queryset(self):
+#     #    result = super(SearchView, self).get_queryset()
+#     #    query = self.request.GET.get('search')
+#     #    if query:
+#     #       postresult = Organisation.objects.filter(name__contains=query)
+#     #       result = postresult
+#     #    else:
+#     #        result = None
+#     #    return result
+
+#     def get_queryset(self): # new
+#         query = self.request.GET.get('q')
+#         if query == None:
+#             object_list = Organisation.objects.all()
+#         else:
+#             object_list = Organisation.objects.filter(
+#                 Q(name__icontains=query)
+#             )
+#         return object_list
