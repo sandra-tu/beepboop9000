df70f8f70098818b366283cd4142e93e02f3ed3c
clairesquires99 <clairesquires99@outlook.com>
2021-10-17 14:33:40 +0100

    requirement.txt

--- register/forms.py
+++ register/forms.py
@@ -2,14 +2,17 @@ from django import forms
 from .models import Organisation
 
 class RegisterForm(forms.ModelForm):
-    # class Meta:
-    #     model = Organisation
-    #     feilds = ('name', 'country', 'city', 'type', 'email')
-    # name = forms.CharField(max_length=255, required=False)
-    # country = forms.CharField(label='Country', widget=forms.Select(choices=COUNTRY_CHOICES))
-    # city = forms.CharField(label='Country', widget=forms.Select(choices=CITY_CHOICES))
-    # type = forms.CharField(label='Country', widget=forms.Select(choices=TYPE_CHOICES))
-    # email = forms.CharField(label='Email', max_length=255, required=True)
     class Meta:
         model = Organisation
         fields = ('name', 'country', 'city', 'type', 'email')
+        widgets = {
+            'email': forms.TextInput(attrs={'placeholder': 'Email address to send incident reports to'}),
+            
+           
+        }
+        labels = {
+            'name': 'Name of your organisation',
+            'type': 'Type of organisation',
+        }
+
+
