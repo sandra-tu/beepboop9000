42557a6ebd2662ac1d03ed412a2135384d08f544
Sandra Tu <sandratu2000@gmail.com>
2021-10-17 10:17:59 +0100

    added date selection widget

--- report/forms.py
+++ report/forms.py
@@ -4,4 +4,8 @@ from .models import Report
 class ReportForm(forms.ModelForm):
     class Meta:
         model = Report
+
         fields = ('date', 'time', 'event_description', 'accused_description', 'name')
+        widgets = {
+            'date': forms.TextInput(attrs={'type': 'date'})
+        }
